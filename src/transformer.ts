e5c1fa3da459b8854857cf20a4b737053b5cbb9a
Bikossor <andre.lichtenthaeler@gmail.com>
2020-09-06 17:38:53 +0200

    Closes #8

---
+++ src/transformer.ts
@@ -0,0 +1,58 @@
+function transformer(tokens: Array<Token>) {
+    let current = 0;
+    let transformed = [];
+
+    transformed.push(
+        `<div style="color:#fff">`
+    );
+
+    while (current < tokens.length) {
+        let token = tokens[current];
+
+        switch (token.type) {
+            case 'name':
+                transformed.push(
+                    `<span style="color:#4FC1FF">${token.value}</span>`
+                );
+
+                current++;
+                break;
+            case 'keyword':
+                transformed.push(
+                    `<span style="color:#499cd5">${token.value}</span>`
+                );
+
+                current++;
+                break;
+            case 'string':
+                transformed.push(
+                    `<span style="color:#ce9178">"${token.value}"</span>`
+                );
+
+                current++;
+                break;
+            case 'number':
+                transformed.push(
+                    `<span style="color:#b5cea8">${token.value}</span>`
+                )
+                current++;
+                break;
+            default:
+                transformed.push(
+                    token.value
+                );
+                current++;
+                break;
+        }
+    }
+
+    transformed.push(
+        `</div>`
+    );
+
+    return transformed;
+}
+
+export {
+    transformer,
+};
