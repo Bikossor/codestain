33ab1514763615cf107bb802ed532468443b1faa
Bikossor <andre.lichtenthaeler@gmail.com>
2020-09-20 14:34:24 +0200

    Closes #31

--- src/tokenizer.ts
+++ src/tokenizer.ts
@@ -296,8 +296,8 @@ function tokenizer(input: string) {
                 char = input[++current];
             } while (char && LETTER.test(char))
 
-            const KEYWORDS = /break|case|catch|class|const|continue|debugger|default|delete|do|else|export|extends|finally|for|function|if|import|in|instanceof|let|new|return|super|switch|this|throw|try|typeof|var|void|while|with|yield/;
-            const BOOLEAN_LITERALS = /true|false/;
+            const KEYWORDS = /^break$|^case$|^catch$|^class$|^const$|^continue$|^debugger$|^default$|^delete$|^do$|^else$|^export$|^extends$|^finally$|^for$|^function$|^if$|^import$|^in$|^instanceof$|^let$|^new$|^return$|^super$|^switch$|^this$|^throw$|^try$|^typeof$|^var$|^void$|^while$|^with$|^yield$/;
+            const BOOLEAN_LITERALS = /^true$|^false$/;
 
             let tokenType = TokenType.Name;
 
