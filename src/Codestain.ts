5451ac3fbf43ac74d4350628e075f95dfc59cca9
André Lichtenthäler <andre.lichtenthaeler@gmail.com>
2022-07-19 18:54:38 +0200

    feat: log parser errors
    #119

--- src/Codestain.ts
+++ src/Codestain.ts
@@ -5,12 +5,14 @@ import { NodeType } from "./enums";
 export const Codestain = (language: InputLanguage, input: string) => {
   const parser = createParser(language);
   // TODO: remove type cast in the future
-  const ast = parser.parse(input).result as unknown as Array<
-    [NodeType, string]
-  >;
+  const parsedInput = parser.parse(input);
+
+  if (parsedInput.isError) console.error(parsedInput.errorMessage);
 
   const transformer = createTransformer("HTML");
-  const output = transformer.transform(ast);
+  const output = transformer.transform(
+    parsedInput.result as unknown as Array<[NodeType, string]>,
+  );
 
   return output;
 };
